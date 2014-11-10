7a793fea2644bf718481dc475d6a8c5fc3962bdb
Roma Milushov <roma@milushov.ru>
2014-11-10 20:07:20 +0300

    yeoman init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aimprApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aimprApp
+ */
+angular.module('aimprApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
